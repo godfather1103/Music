b82382571d816435de103a4f295585e48ff550e3
godfather1103 <chuchuanbao@gmail.com>
2016-06-22 21:58:06 +0800

    修改了网络请求方式

--- app/src/main/java/com/demo/ccb/util/API.java
+++ app/src/main/java/com/demo/ccb/util/API.java
@@ -2,29 +2,22 @@ package com.demo.ccb.util;
 
 import android.util.Log;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-//import java.net.HttpURLConnection;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+
 
 
 /**
@@ -35,42 +28,19 @@ import java.util.List;
  */
 public class API {
 
-    //private HttpURLConnection connection = null;
     private final ArrayList<NameValuePair> headerList = new ArrayList<>();
-    //   private ArrayList<NameValuePair> cookies = new ArrayList<NameValuePair>();
-//    int default_timeout = 10;
-//    String modulus = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7";
-//    String nonce = "0CoJUm6Qyw8W8jud";
-//    String pubKey = "010001";
 
     public API() {
         headerList.add(new BasicNameValuePair("Accept", "*/*"));
         headerList.add(new BasicNameValuePair("Accept-Language", "zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4"));
         headerList.add(new BasicNameValuePair("Connection", "keep-alive"));
         headerList.add(new BasicNameValuePair("Content-Type", "application/x-www-form-urlencoded"));
-        headerList.add(new BasicNameValuePair("Host", "music.163.com"));
-        headerList.add(new BasicNameValuePair("Referer", "http://music.163.com/search/"));
         headerList.add(new BasicNameValuePair("User-Agent",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"));
-        //cookies.add(new BasicNameValuePair("appver", "1.5.2"));
+        headerList.add(new BasicNameValuePair("Host", "music.163.com"));
+        headerList.add(new BasicNameValuePair("Referer", "http://music.163.com/search/"));
     }
 
-    // 歌曲加密算法, 基于https://github.com/yanunon/NeteaseCloudMusic脚本修改后实现
-//    public String encrypted_id(long id) throws Exception {
-//        MessageDigest md5 = MessageDigest.getInstance("MD5");
-//       // BASE64Encoder base64en = new BASE64Encoder();
-//        String result = null;
-//       byte[] magic = "3go8&$8*3*3h0k(2)2".getBytes();
-//        byte[] song_id = new byte[(int) id];
-//       int magic_len = magic.length;
-//        for (int i = 0; i < song_id.length; i++) {
-//            song_id[i] = (byte) (song_id[i] ^ magic[i % magic_len]);
-//        }
-//       // result = base64en.encode(md5.digest(song_id));
-//        result = result.replace('/', '_');
-//        result = result.replace('+', '-');
-//        return result;
-//    }
 
     public JSONObject search(String s, int type, int offset, String total, int limit) throws Exception {
 
@@ -101,7 +71,7 @@ public class API {
     *
     * */
     public String rawHttpRequest(String method, String action, List<NameValuePair> query) throws Exception {
-        String result = null;
+ /*       String result = null;
         HttpClient httpClient = new DefaultHttpClient();
         HttpResponse httpResponse = null;
         if ("GET".equals(method)) {
@@ -123,6 +93,55 @@ public class API {
         }
         if (httpResponse != null && httpResponse.getStatusLine().getStatusCode() == 200) {
             result = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+        }*/
+
+
+        String result = null;
+        StringBuffer Params = new StringBuffer();
+        byte[] buffer = new byte[4 * 1024];
+        int len = -1;
+        StringBuffer stringBuffer = new StringBuffer();
+
+        URLConnection conn = new URL(action).openConnection();
+        conn.setReadTimeout(4000);
+        for (int i = 0; i < headerList.size(); i++) {
+            conn.setRequestProperty(headerList.get(i).getName(), headerList.get(i).getValue());
+        }
+
+        //得到输入流
+        BufferedInputStream bis = null;
+
+        if ("GET".equals(method)){
+            bis = new BufferedInputStream(conn.getInputStream());
+            while ((len = bis.read(buffer)) != -1) {
+                stringBuffer.append(new String(buffer,0,len));
+            }
+            result = stringBuffer.toString();
+        }else if ("POST".equals(method)&&query!=null){
+
+            Params.append("1").append("=").append("1");
+
+            for (NameValuePair param:
+                    query) {
+                Params.append("&").append(param.getName()).append("=").append(param.getValue());
+            }
+
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            //发送参数
+            PrintWriter out = null;
+            out = new PrintWriter(conn.getOutputStream());
+            out.print(Params.toString());
+            out.flush();
+
+
+            bis = new BufferedInputStream(conn.getInputStream());
+            while ((len = bis.read(buffer)) != -1) {
+                stringBuffer.append(new String(buffer,0,len));
+            }
+            result = stringBuffer.toString();
         }
         return result;
     }
@@ -153,13 +172,9 @@ public class API {
         //设置超时间为4秒
         conn.setConnectTimeout(4 * 1000);
         //防止屏蔽程序抓取而返回403错误
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             conn.setRequestProperty(headerList.get(i).getName(), headerList.get(i).getValue());
         }
-
-
-        conn.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36");
 
         //得到输入流
         BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
@@ -182,8 +197,6 @@ public class API {
             bis.close();
         if (bos != null)
             bos.close();
-
-        //return true;
     }
 
     //从网络上下载歌词
@@ -202,6 +215,5 @@ public class API {
         bos.write(bytes, 0, bytes.length);
         bos.flush();
         bos.close();
-        //return true;
     }
 }
